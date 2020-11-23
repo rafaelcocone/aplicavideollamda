45de40db26b335c37ef02d270759931ef4c189d2
rafael cocone <rafaelcocone@gmail.com>
2020-11-23 10:46:11 -0600

    actualzaicon 23-11-2020

--- src/index.js
+++ src/index.js
@@ -80,6 +80,7 @@ app.use((req,res,next) => {
 app.use(require('./routes'))
 app.use(require('./routes/authentication'))
 app.use('/links',require('./routes/links'))
+app.use('/agenda',require('./routes/agenda'))
 app.use('/peerjs',peerServer)
 app.use('/videollamada',require('./routes/videollamada')(io))
 
---
+++ src/routes/agenda.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const   express     = require('express'),
+        router      = express.Router(),
+        pool        = require('../database')
+const  {isLoggedIn} = require('../lib/auth')//verificar si esta autentificado y loggeado
+
+
+router.get('/',isLoggedIn, async(req,res) => {
+    var sql = 'SELECT  * FROM tickets '  
+        sql += 'WHERE id_user = ? OR id_experto = ?'
+
+    const tickets = await pool.query(sql, [req.user.id, req.user.id] )
+    res.render('agenda/list', {tickets: tickets})
+})
+
+
+module.exports = router
